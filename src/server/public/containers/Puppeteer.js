713570d407d29471850cb714ffccc0bd4e980822
Yamashita-Yoshimasa <yychbook040720@gmail.com>
2021-11-10 01:11:49 +0900

    server build

--- src/server/public/containers/Puppeteer.js
+++ src/server/public/containers/Puppeteer.js
@@ -14,33 +14,48 @@ var __async = (__this, __arguments, generator) => {
         reject(e);
       }
     };
-    var step = (x) => x.done ? resolve(x.value) : Promise.resolve(x.value).then(fulfilled, rejected);
+    var step = (x) =>
+      x.done
+        ? resolve(x.value)
+        : Promise.resolve(x.value).then(fulfilled, rejected);
     step((generator = generator.apply(__this, __arguments)).next());
   });
 };
-import puppeteer from "puppeteer";
-import dotenv from "dotenv";
-const OpenPage = () => __async(void 0, null, function* () {
-  let pckoubouNames = [];
-  let pckoubouPrices = [];
-  let pckoubouId = [];
-  dotenv.config();
-  const browser = yield puppeteer.launch({
-    headless: true
+import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+const OpenPage = () =>
+  __async(void 0, null, function* () {
+    let pckoubouNames = [];
+    let pckoubouPrices = [];
+    let pckoubouId = [];
+    dotenv.config();
+    const browser = yield puppeteer.launch({
+      headless: true,
+    });
+    const page = yield browser.newPage();
+    yield page.goto(process.env.PC_KOUBOU_URL || '', {
+      waitUntil: 'networkidle2',
+      timeout: 0,
+    });
+    pckoubouNames = yield page.$$eval('.name', (item) =>
+      item.map((names) => names.textContent),
+    );
+    pckoubouPrices = yield page.$$eval('.price--num', (item) =>
+      item.map((prices) => prices.textContent),
+    );
+    pckoubouId = yield page.$$eval('.item-code > dd', (item) =>
+      item.map((id) => id.textContent),
+    );
+    yield browser.close();
+    if (
+      typeof pckoubouNames === 'object' &&
+      pckoubouNames != null &&
+      typeof pckoubouPrices === 'object' &&
+      pckoubouPrices != null &&
+      typeof pckoubouId === 'object' &&
+      pckoubouId != null
+    ) {
+      return Promise.resolve([pckoubouNames, pckoubouPrices, pckoubouId]);
+    }
   });
-  const page = yield browser.newPage();
-  yield page.goto(process.env.PC_KOUBOU_URL || "", {
-    waitUntil: "networkidle2",
-    timeout: 0
-  });
-  pckoubouNames = yield page.$$eval(".name", (item) => item.map((names) => names.textContent));
-  pckoubouPrices = yield page.$$eval(".price--num", (item) => item.map((prices) => prices.textContent));
-  pckoubouId = yield page.$$eval(".item-code > dd", (item) => item.map((id) => id.textContent));
-  yield browser.close();
-  if (typeof pckoubouNames === "object" && pckoubouNames != null && typeof pckoubouPrices === "object" && pckoubouPrices != null && typeof pckoubouId === "object" && pckoubouId != null) {
-    return Promise.resolve([pckoubouNames, pckoubouPrices, pckoubouId]);
-  }
-});
-export {
-  OpenPage
-};
+export { OpenPage };
